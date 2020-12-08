7ccca70495c80adfab23d2b9ad14665e40696e9a
Acer Aspire <khanhlinh1475@gmail.com.com>
2020-12-08 11:06:58 +0700

    hoan thanh bai tap 1 2 3

---
+++ Excercise/Lab4/BaiTap3/app/src/main/java/com/example/baitap3/DrawWithPath.java
@@ -0,0 +1,57 @@
+package com.example.baitap3;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class DrawWithPath extends View {
+    public Paint paint = new Paint();
+    public Path path = new Path();
+
+    public DrawWithPath(Context context) {
+        super(context);
+    }
+
+    public DrawWithPath(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        //paint.setAntiAlias(true);
+        paint.setStrokeWidth(5f);
+        paint.setColor(Color.WHITE);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        float eventX = event.getX();
+        float eventY = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(eventX, eventY);
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(eventX, eventY);
+                break;
+            default:
+                return false;
+        }
+
+        invalidate(); //làm mất hiệu lực
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawPath(path, paint);
+    }
+}
